06e1df3bf82204fc41071cd4b3742a6de3649b0d
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2023-11-20 17:47:07 +0200

    F

---
+++ src/Lab2/Dispatcher.java
@@ -0,0 +1,1 @@
+package Lab2;class Dispatcher {    public static double calculateArea(Figure figure) {        return figure.calculateArea();    }    public static double calculateVolume(Figure figure) {        return figure.calculateVolume();    }}
