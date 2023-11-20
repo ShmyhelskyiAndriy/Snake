1570db771c6ae97309d01b03be415fc9012c16d4
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2023-11-20 18:07:26 +0200

    Update Dispatcher.java

--- src/Lab2/Dispatcher.java
+++ src/Lab2/Dispatcher.java
@@ -1,1 +1,11 @@
-package Lab2;class Dispatcher {    public static double calculateArea(Figure figure) {        return figure.calculateArea();    }    public static double calculateVolume(Figure figure) {        return figure.calculateVolume();    }}
+package Lab2;
+
+class Dispatcher {
+    public static double calculateArea(Figure figure) {
+        return figure.calculateArea();
+    }
+  
+    public static double calculateVolume(Figure figure) {
+        return figure.calculateVolume();
+    }
+}
