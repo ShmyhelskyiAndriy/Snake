bff839ac406bd7d355a4f7a846ae5be1144103a7
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2023-11-20 18:09:03 +0200

    Update Figure.java

--- src/Lab2/Figure.java
+++ src/Lab2/Figure.java
@@ -1,1 +1,35 @@
-package Lab2;abstract class Figure {    private double length;    private double width;    private double height;    abstract double calculateArea();    abstract double calculateVolume();    public double getLength() {        return length;    }    public void setLength(double length) {        this.length = length;    }    public double getWidth() {        return width;    }    public void setWidth(double width) {        this.width = width;    }    public double getHeight() {        return height;    }    public void setHeight(double height) {        this.height = height;    }}
+package Lab2;
+
+abstract class Figure {
+    private double length;
+    private double width;
+    private double height;
+
+    abstract double calculateArea();
+
+    abstract double calculateVolume();
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
