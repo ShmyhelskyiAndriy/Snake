c7c0b4255057583fdc8cca629867eace7a4bdd84
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2023-12-22 11:34:37 +0200

    gg

--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args) {
         double square = (new Triangle()).getSquare(6);
         double square2 = (new Cube()).getSquare(2);
-        double half = (new Cube()).getHalf(5 );
+        double half = (new Cube()).getHalf(5);
         System.out.println(square);
         System.out.println(square2);
         System.out.println(half);
