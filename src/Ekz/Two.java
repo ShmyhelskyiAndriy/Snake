19e80bd5e17873ffed4f865eb5a80b68a8ccfecd
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-01-16 13:43:08 +0200

    gg

--- src/Ekz/Two.java
+++ src/Ekz/Two.java
@@ -1,1 +1,13 @@
-package Ekz;public class Two extends One{    int j;    public Two(int j){        this.j=j;    }    int sum(){        int sum = super.j + j;        return sum;    }}
+package Ekz;
+
+public class Two extends One{
+    int j;
+    public Two(int j){
+        this.j=j;
+    }
+  
+    int sum(){
+        int sum = super.j + j;
+        return sum;
+    }
+}
