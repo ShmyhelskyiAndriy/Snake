ed0d5c3f98f2054532dade007471e565211ece34
ShmyhelskyiAndriy <126695567+ShmyhelskyiAndriy@users.noreply.github.com>
2024-01-16 13:42:22 +0200

    gg

--- src/Ekz/Factorial.java
+++ src/Ekz/Factorial.java
@@ -1,1 +1,13 @@
-package Ekz;public class Factorial {    public static int factorial(int n) {        if (n == 0) {            return 1;        } else {            return n * factorial(n - 1);        }    }}
+package Ekz;
+
+public class Factorial {
+    public static int factorial(int n) {
+      
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+
+    }
+}
